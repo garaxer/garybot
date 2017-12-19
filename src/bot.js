b50592a595788313f3f1bf89f270936724803dac
Ryan Pollard <ryan@jeremy.ephox.intra>
2017-12-19 17:03:14 +1000

    Added more stuff.

--- src/bot.js
+++ src/bot.js
@@ -11,6 +11,20 @@ gary.on("message", message => {
   if (message.content == "test") {
     message.channel.send("It's me, Gary!")
   }
+
+  if (message.content == "What's the time?") {
+    const today = new Date();
+    const h = today.getHours();
+    const m = today.getMinutes();
+    const s = today.getSeconds();
+    const time = h + ":" + m;
+    message.channel.send("It's " + time + " and " + s + " seconds.")
+  }
+
+  if (message.content == "Thanks Gary.") {
+    message.channel.send("You're welcome, " + message.member + ".")
+  }
+
 });
 
 gary.login(auth.token)
