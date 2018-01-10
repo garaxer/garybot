a416b9ed75b29fa5e0c36f40d8ff88b4d8724b49
Gary <gbagnall8@gmail.com>
2018-01-10 22:41:31 +1000

    got changes from master

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,7 @@
-const http = require("axios");
-const auth = require("../auth.json");
-
+const http   = require("axios");
+const auth   = require("../auth.json");
+const coords = require("random-coordinates");
+const gary   = require("./gary.js").feats;
 
 const prefix = "gary,";
 const suffix = "gary";
@@ -20,6 +21,33 @@ const tldr =
   "&SM_WITH_BREAK" +
   "&SM_LENGTH=4" +
   "&SM_URL=";
+
+const chan = (board) => "https://a.4cdn.org" + board + "catalog.json"
+
+const sortChan = (board) => {
+  var sorted = [];
+  for (i in board) {
+    const threads = board[i].threads
+    for (j in threads) {
+      sorted.push({
+        no      : threads[j].no,
+        replies : threads[j].replies
+      });
+    }
+  }
+
+  sorted.sort((a,b) => {
+    let comparison = 0;
+    if (a.replies > b.replies) {
+      comparison = -1;
+    } else if (a.replies < b.replies) {
+      comparison = 1;
+    }
+    return comparison;
+  });
+
+  return sorted;
+}
 
 exports.getMap = (coords) =>
   "https://maps.googleapis.com/maps/api/geocode/json" +
@@ -41,7 +69,9 @@ exports.hasSuffix = msg => {
   return splitSuffixMessage(msg).suffix.includes(suffix);
 }
 
-exports.advCheck = (func, cmd) => {
+exports.featSearch = cmd => Promise.resolve(gary.filter( f => this.featCheck(f.cmd, cmd))[0])
+
+exports.featCheck = (func, cmd) => {
   return cmd.split(" ").slice(0, func.split(" ").length).join(" ") == func;
 }
 
@@ -70,8 +100,38 @@ exports.search = (type, query) => {
         .then(response => (response.data.sm_api_content))
         .then(summary => summary.replace(/\[BREAK\]/g, "\n\n"))
         .catch(console.error)
+      break;
+
+    case "randomPlace":
+      return http.get(this.getMap(coords({ fixed: 7 }).split(" ").join("")))
+        .then(response => {
+          if (response.data.status == "OK") {
+            const place = response.data.results[0];
+            return "I think I'm in "
+            + place.formatted_address + "\n"
+            + "https://www.google.com.au/maps/place/"
+            + place.geometry.location.lat + ","
+            + place.geometry.location.lng
+          } else {
+            return this.search("randomPlace")
+          }
+        })
+        .catch(console.error)
+      break;
+
+    case "4chan":
+      return http.get(chan(query))
+        .then(response => response.data)
+        .then(boards => sortChan(boards)[0])
+        .then(thread =>
+          "This thread has the most replies on " + query + ":\n" +
+          "http://boards.4chan.org" + query + "thread/" + thread.no)
+        .catch(console.error)
+      break;
   }
 }
+
+exports.isTheMan = (id) => (id == "186723484699721728" || id == "182083904545488896");
 
 exports.log = (user, task) => {
   console.log("User " + user + " is " + task);
