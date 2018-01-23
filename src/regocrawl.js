a9820da5ed9eba12ec35fd940169360f0a5c46e0
Gary <gbagnall8@gmail.com>
2018-01-23 20:35:20 +1000

    Add qld rego web crawler as a test

--- src/regocrawl.js
+++ src/regocrawl.js
@@ -4,12 +4,16 @@ const ACCEPT_SELECTOR = '#tAndCForm\\:confirmButton';
 const REGO_SELECTOR = '#vehicleSearchForm\\:plateNumber';
 const SEARCH_SELECTOR = '#vehicleSearchForm\\:confirmButton';
 const REGISTERED_SELECTOR = '#j_id_2a > dl:nth-child(2) > dd:nth-child(2)';
-(async () => {
-  const browser = await puppeteer.launch();
-  //const browser = await puppeteer.launch({headless: false}); //to watch what it  does
+
+
+//async function run(query) {
+//const run = async query => {
+exports.search = async query => {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false}); //to watch what it  does
   const page = await browser.newPage();
   await page.goto('https://www.service.transport.qld.gov.au/checkrego/application/VehicleSearch.xhtml');
-  await page.waitFor(2000);
+  await page.waitFor(1000);
   await page.click(ACCEPT_SELECTOR);
   await page.waitForNavigation();
   await page.click(REGO_SELECTOR);
@@ -20,9 +24,13 @@ const REGISTERED_SELECTOR = '#j_id_2a > dl:nth-child(2) > dd:nth-child(2)';
     let element = document.querySelector(sel);
     return element? element.innerHTML: null;
   }, REGISTERED_SELECTOR);
-
-  console.log("Vehicle is " + registered);
-  await page.screenshot({path: 'registered.png'});
+  //console.log("Vehicle is " + registered);
+  //await page.screenshot({path: 'registered.png'});
   await browser.close();
-  return registered;
-})();
+
+  return (registered)
+   ? query + " is " + registered.toLowerCase()+"."
+   : query + " is not registered or found.";
+}
+
+//exports.search = query =>  run(query);
