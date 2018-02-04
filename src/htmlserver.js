050729a2d91f93faf8a7d95107adcc1f68db8a5c
Gary <gbagnall8@gmail.com>
2018-02-04 22:38:18 +1000

    read directory addded

--- src/htmlserver.js
+++ src/htmlserver.js
@@ -7,7 +7,8 @@ const util = require('util');
 
 module.exports.createhtmlSever = bot => {
 //from w3 schools
-  const textChannelToSendTo = "395554251729928192";
+  //const textChannelToSendTo = "395554251729928192";
+  const textChannelToSendTo = "380251833366740992";
   const server = http.createServer((req, res) => {
     if (req.method.toLowerCase() == 'get') {
       displayForm(res);
