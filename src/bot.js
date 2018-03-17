b3146201b2ef2ce80a8aecbfcad9d18bd7153c86
Gary <gbagnall8@gmail.com>
2018-03-17 16:31:33 +1000

    Add changes from master

---
+++ src/bot.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const auth    = require("../auth.json");
+const lib     = require("./lib.js");
+
+const sendMsg = require("./htmlserver.js");
+
+const bot     = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log("Reactor online.");
+  console.log("Sensors online.");
+  console.log("Weapons online.");
+  console.log("All systems nominal.");
+  sendMsg.createhtmlSever(bot);
+});
+
+bot.on("message", message => {
+  if (lib.isCommand(message)) {
+
+    const ch      = message.channel;
+    const user    = message.author.username;
+    const command = lib.getCommand(message)
+
+    lib.featSearch(command)
+      .then(feat => {
+        const params = lib.getParams(feat.cmd, command);
+        lib.log(user, feat.log(params));
+        return feat.func(params, message.author.id);
+      })
+      .then(msg => ch.send(msg))
+      .catch(error => {
+        console.error(user + " entered an invalid command - " + command);
+        ch.send("What the fuck is a " + "\"" + command + "\"" + " lmao.");
+      })
+  }
+
+});
+
+bot.login(auth.token);
---
+++ src/htmlserver.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+const formidable = require("formidable");
+const util = require('util');
+
+
+module.exports.createhtmlSever = bot => {
+//from w3 schools
+  const textChannelToSendTo = "395554251729928192";
+  const server = http.createServer((req, res) => {
+    if (req.method.toLowerCase() == 'get') {
+      displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+  });
+
+  const displayForm = res => {
+    fs.readFile('niceform.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html',
+      'Content-Length': data.length
+      });
+      res.write(data);
+      res.end();
+    });
+  }
+
+  const processAllFieldsOfTheForm = (req, res) => {
+      var form = new formidable.IncomingForm();
+
+      form.parse(req,  (err, fields, files) => {
+          console.log(fields.stuff);
+          bot.channels.get(textChannelToSendTo).send(fields.stuff);
+      });
+  }
+
+  server.listen(8080);
+}
